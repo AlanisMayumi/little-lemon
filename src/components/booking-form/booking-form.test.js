7ec6f7cc7e1da63876837f4e5a28753e0af6a5ae
AlanisMayumi <mayumi.alanis@gmail.com>
2026-06-12 22:09:31 +0900

    adding booking form unit tes

---
+++ src/components/booking-form/booking-form.test.js
@@ -0,0 +1,28 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import BookingForm from "./index";
+
+test("Renders the BookingForm heading", () => {
+  render(<BookingForm />);
+  const headingElement = screen.getByText("Make Your reservation");
+  expect(headingElement).toBeInTheDocument();
+});
+
+describe("BookingForm", () => {
+  it("should execute onSubmit callback with correct data when form is submitted", () => {
+    const mockOnSubmit = jest.fn();
+    render(<BookingForm onSubmit={mockOnSubmit} />);
+
+    const dateInput = screen.getByLabelText(/choose date/i);
+    const timeSelect = screen.getByLabelText(/choose time/i);
+    const submitButton = screen.getByText(/make your reservation/i);
+
+    fireEvent.change(dateInput, { target: { value: "2024-07-01" } });
+    fireEvent.change(timeSelect, { target: { value: "18:00" } });
+    fireEvent.click(submitButton);
+
+    expect(mockOnSubmit).toHaveBeenCalledWith({
+      date: "2024-07-01",
+      time: "18:00",
+    });
+  });
+});
