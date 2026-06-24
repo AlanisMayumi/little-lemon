3575fb92a57b683fe7a392b6a687abcbaa219933
AlanisMayumi <mayumi.alanis@gmail.com>
2026-06-24 22:17:49 +0900

    fix react router dom unit test v7 issue

--- src/pages/booking/index.jsx
+++ src/pages/booking/index.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 
 import Footer from "../../components/Footer.js";
 import Header from "../../components/Header.js";
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,14 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+// reference: https://stackoverflow.com/questions/79332161/referenceerror-textencoder-is-not-defined-after-react-router-dom-upgrade-to-7
+import { TextEncoder, TextDecoder } from "node:util";
+
+if (!global.TextEncoder) {
+  global.TextEncoder = TextEncoder;
+}
+
+if (!global.TextDecoder) {
+  global.TextDecoder = TextDecoder;
+}
