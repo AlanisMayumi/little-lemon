d377a1c56d78059fa0bc28c69cae07f865bbfc89
AlanisMayumi <mayumi.alanis@gmail.com>
2026-06-29 22:15:33 +0900

    feat: adding formik

--- src/components/booking-form/index.jsx
+++ src/components/booking-form/index.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import { timeOptions } from "../../hooks/useBooking";
+import { Formik } from "formik";
+
+const errorMessage = (field) => <div style={{ color: "red" }}>{field}</div>;
 
 const BookingForm = ({ onSubmit }) => {
+  const today = new Date();
   const [date, setDate] = useState("");
   const [time, setTime] = useState();
   const handleSubmit = (e) => {
@@ -14,51 +18,74 @@ const BookingForm = ({ onSubmit }) => {
 
   return (
     <>
-      <form
-        style={{ display: "grid", maxWidth: "200px", gap: "20px" }}
+      <Formik
         onSubmit={handleSubmit}
+        validate={(values) => {
+          const errors = {};
+
+          if (!values.date) {
+            errors.date = "Date is required";
+          }
+
+          return errors;
+        }}
       >
-        <label htmlFor="res-date">Choose date</label>
-        <input
-          type="date"
-          name="date"
-          id="res-date"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
-        <label htmlFor="res-time">Choose time</label>
-        <select
-          name="time"
-          id="res-time"
-          value={time}
-          onChange={(e) => {
-            console.log("Selected time:", e.target.value);
-            setTime(e.target.value);
-          }}
-        >
-          {timeOptions.map((time) => (
-            <option key={time} value={time}>
-              {time}
-            </option>
-          ))}
-        </select>
-        <label htmlFor="guests">Number of guests</label>
-        <input
-          type="number"
-          placeholder="1"
-          min="1"
-          defaultValue="1"
-          max="10"
-          id="guests"
-          name="guests"
-        />
-        <label htmlFor="occasion">Occasion</label>
-        <select id="occasion" name="occasion">
-          <option value="birthday">Birthday</option>
-          <option value="anniversary">Anniversary</option>
-        </select>
-        <input type="submit" value="Make Your reservation" />
-      </form>
+        {({ handleSubmit, errors, values, isSubmitting }) => (
+          <form
+            onSubmit={handleSubmit}
+            style={{ display: "grid", maxWidth: "200px", gap: "20px" }}
+          >
+            <label htmlFor="res-date">Choose date</label>
+            <input
+              type="date"
+              name="date"
+              id="res-date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+            {errors.date && errorMessage(errors.date)}
+            <label htmlFor="res-time">Choose time</label>
+            <select
+              name="time"
+              id="res-time"
+              value={time}
+              onChange={(e) => {
+                console.log("Selected time:", e.target.value);
+                setTime(e.target.value);
+              }}
+            >
+              {timeOptions.map((time) => (
+                <option key={time} value={time}>
+                  {time}
+                </option>
+              ))}
+            </select>
+            {errors.time && errorMessage(errors.time)}
+            <label htmlFor="guests">Number of guests</label>
+            <input
+              type="number"
+              placeholder="1"
+              min="1"
+              defaultValue="1"
+              max="10"
+              id="guests"
+              name="guests"
+            />
+            {errors.guests && errorMessage(errors.guests)}
+            <label htmlFor="occasion">Occasion</label>
+            <select id="occasion" name="occasion">
+              <option value="birthday">Birthday</option>
+              <option value="anniversary">Anniversary</option>
+            </select>
+            {errors.occasion && errorMessage(errors.occasion)}
+            <input
+              type="submit"
+              value="Make Your reservation"
+              disabled={isSubmitting}
+            />
+          </form>
+        )}
+      </Formik>
     </>
   );
 };
